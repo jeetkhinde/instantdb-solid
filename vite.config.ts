716baf408518802ebbfb13e2ae75b4a0141cb045
Jagjeet Singh <jeetkhinde@icloud.com>
2025-04-10 18:36:23 -0400

    improved naming of the package

--- vite.config.ts
+++ vite.config.ts
@@ -6,26 +6,10 @@ export default defineConfig( {
     extensions: [ '.js', '.ts', '.jsx', '.tsx' ],
   },
   optimizeDeps: {exclude: [ 'fsevents' ]},
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    transformMode: {
-      web: [ /\.[jt]sx?$/ ],
-    },
-    // solid needs to be inline to work around
-    // a resolution issue in vitest
-    deps: {
-      inline: [ /solid-js/ ],
-    },
-    // if you have few tests, try commenting one
-    // or both out to improve performance:
-    threads: false,
-    isolate: false,
-  },
   build: {
     lib: {
       entry: path.resolve( __dirname, 'src/index.ts' ),
-      name: 'InstantDBSolid',
+      name: '@jeetkhinde/instantdb-solid',
       fileName: ( format ) => `instantdb-solid.${format}.js`,
     },
     rollupOptions: {
@@ -39,3 +23,21 @@ export default defineConfig( {
     },
   },
 } );
+
+/// Vitest configuration
+export const test = {
+  globals: true,
+  environment: 'jsdom',
+  transformMode: {
+    web: [ /\.[jt]sx?$/ ],
+  },
+  // solid needs to be inline to work around
+  // a resolution issue in vitest
+  deps: {
+    inline: [ /solid-js/ ],
+  },
+  // if you have few tests, try commenting one
+  // or both out to improve performance:
+  threads: false,
+  isolate: false,
+};
